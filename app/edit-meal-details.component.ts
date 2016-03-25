0334216ad19c0bbb2b9bdfdc582b76d6c80b89b8
browneryan <browneryan87@gmail.com>
2016-03-25 11:57:42 -0700

    Add edit-meal-details.component.ts, ability to edit name, details, calories

---
+++ app/edit-meal-details.component.ts
@@ -0,0 +1,18 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'edit-meal-details',
+  inputs: ['meal'],
+  template: `
+    <div class="meal-form">
+      <h3>Edit Description:</h3>
+      <input [(ngModel)]="meal.name" class="meal-form"/>
+      <input [(ngModel)]="meal.details" class="meal-form"/>
+      <input [(ngModel)]="meal.calories" class="meal-form"/>
+    </div>
+  `
+})
+export class EditMealDetailsComponent {
+  public meal: Meal;
+}
