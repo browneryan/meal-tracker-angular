108541c9f9a51a96b7c5ede4ab67a9d75aa7a3a2
browneryan <browneryan87@gmail.com>
2016-03-25 12:00:27 -0700

    Import EditMealDetailsComponent to app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 import { MealListComponent } from './meal-list.component';
 import { MealComponent } from './meal.component';
+import { EditMealDetailsComponent } from './edit-meal-details.component';
 
 @Component({
   selector: 'my-app',
