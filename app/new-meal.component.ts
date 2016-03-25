b40f1b05608d63905f90584f57ca03483727de1d
browneryan <browneryan87@gmail.com>
2016-03-25 13:59:58 -0700

    Add new-meal.component.ts, ability to add a new meal works

---
+++ app/new-meal.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'new-meal',
+  outputs: ['onSubmitNewMeal'],
+  template: `
+  <div class="meal-form">
+    <h3>Create Meal:</h3>
+    <input placeholder="Name" class="col-sm-8 input-lg" #newName>
+    <input placeholder="Details" class="col-sm-8 input-lg" #newDetails>
+    <input placeholder="Calories" type="number" class="col-sm-8 input-lg" #newCalories>
+    <button (click)="addMeal(newName, newDetails, newCalories)">Add</button>
+  </div>
+  `
+})
+export class NewMealComponent {
+  public onSubmitNewMeal: EventEmitter<String[]>;
+  constructor(){
+    this.onSubmitNewMeal = new EventEmitter();
+  }
+  addMeal(newName: HTMLInputElement, newDetails: HTMLInputElement, newCalories: HTMLInputElement){
+    var newMeal = [newName.value, newDetails.value, newCalories.value];
+    this.onSubmitNewMeal.emit(newMeal);
+    newName.value = "";
+    newDetails.value = "";
+    newCalories.value = "";
+    console.log("Creating meal...")
+  }
+}
