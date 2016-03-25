2eb4d4e3f0d0797a9e52fc67dbd5e264bbaed659
browneryan <browneryan87@gmail.com>
2016-03-25 09:20:54 -0700

    Add basic app component structure, names of meals display correctly.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
       <h1>Meal Tracker</h1>
+      <h3 *ngFor="#meal of meals">{{ meal.name }}</h3>
     <div>
   `
 })
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Vegan Burger", "Ordered a salad on the side instead of fries.", 375),
+      new Meal("Falafel", "Didn't order feta cheese.", 375),
+      new Meal("Whole Bowl", "I skipped the sour cream and cheese.", 450)];
+  }
 }
