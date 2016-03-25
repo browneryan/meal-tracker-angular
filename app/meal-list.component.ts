741ca94275315473c7946fed8340b70fd5047c2b
browneryan <browneryan87@gmail.com>
2016-03-25 14:00:25 -0700

    Add new-meal selector to meal-list.component.ts

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -2,12 +2,14 @@ import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 import { MealComponent } from './meal.component';
 import { EditMealDetailsComponent } from './edit-meal-details.component';
+import { NewMealComponent } from './new-meal.component';
+
 
 @Component({
 selector: 'meal-list',
 inputs: ['mealList'],
 outputs: ['onMealSelect'],
-directives: [MealComponent, EditMealDetailsComponent],
+directives: [MealComponent, EditMealDetailsComponent, NewMealComponent],
 template: `
   <meal-display *ngFor="#currentMeal of mealList"
     (click)="mealClicked(currentMeal)"
@@ -16,6 +18,7 @@ template: `
   </meal-display>
   <edit-meal-details *ngIf="selectedMeal" [meal]="selectedMeal">
   </edit-meal-details>
+  <new-meal (onSubmitNewMeal)="createMeal($event)" ></new-meal>
 `
 })
 export class MealListComponent {
@@ -28,5 +31,10 @@ export class MealListComponent {
   mealClicked(clickedMeal: Meal): void {
     this.onMealSelect.emit(clickedMeal);
     this.selectedMeal = clickedMeal;
+  }
+  createMeal(newMealInfo): void {
+    this.mealList.push(
+      new Meal(newMealInfo[0], newMealInfo[1], newMealInfo[2])
+    );
   }
 }
