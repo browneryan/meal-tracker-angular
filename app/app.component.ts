fb0c7d88f3c0697b9faa8b1c6b15a8f7de3351b8
browneryan <browneryan87@gmail.com>
2016-03-25 10:44:37 -0700

    Remove html template, add meal-list selector, mealWasSelected method added to AppComponent class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
+import { MealListComponent } from './meal-list.component';
 
 @Component({
   selector: 'my-app',
+  directives: [MealListComponent],
   template: `
     <div class="container">
       <h1>Meal Tracker</h1>
-        <div *ngFor="#meal of meals" (click)="mealWasSelected(meal)">
-          <h3 >{{ meal.name }}</h3>
-        </div>
-    <div>
+      <meal-list
+        [mealList]="meals"
+        (onMealSelect)="mealWasSelected($event)">
+      </meal-list>
+    </div>
   `
 })
 export class AppComponent {
@@ -18,9 +21,10 @@ export class AppComponent {
     this.meals = [
       new Meal("Vegan Burger", "Ordered a salad on the side instead of fries.", 375),
       new Meal("Falafel", "Didn't order feta cheese.", 375),
-      new Meal("Whole Bowl", "I skipped the sour cream and cheese.", 450)];
+      new Meal("Whole Bowl", "I skipped the sour cream and cheese.", 450)
+    ];
   }
-  mealWasSelected(clickedMeal: Meal):void {
-    console.log(clickedMeal);
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log('parent', clickedMeal);
   }
 }
